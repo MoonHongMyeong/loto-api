fbf107bb2b777fafa0d04a1b6e5e11fd72c3cf48
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-11-14 01:35:53 +0900

    chore: Jpa 설정 추가 [#2]

---
+++ src/main/java/gg/loto/common/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package gg.loto.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
