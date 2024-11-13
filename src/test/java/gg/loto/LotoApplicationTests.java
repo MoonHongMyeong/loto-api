1521bfa32e01c6cb60bd4ae30ecfb3b1239fb953
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-11-14 00:22:48 +0900

    project init

---
+++ src/test/java/gg/loto/LotoApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.loto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
