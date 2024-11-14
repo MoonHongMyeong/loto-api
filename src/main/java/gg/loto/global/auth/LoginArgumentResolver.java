29ce36dedf3de3af982eaab6c635e96bbfe4087a
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-11-14 17:11:04 +0900

    Add: 파라미터에서 어노테이션으로 세션 정보를 관리하기 위한LoginArgumentResolver 구현 [#3]

---
+++ src/main/java/gg/loto/global/auth/LoginArgumentResolver.java
@@ -0,0 +1,35 @@
+package gg.loto.global.auth;
+
+import gg.loto.global.auth.dto.SessionUser;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class LoginArgumentResolver implements HandlerMethodArgumentResolver {
+    private static final String SESSION_KEY = "USER";
+    private final HttpSession session;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isSessionUserClass = SessionUser.class.equals(parameter.getParameterType());
+
+        return isLoginUserAnnotation && isSessionUserClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        Object sessionUser = session.getAttribute(SESSION_KEY);
+        if (sessionUser == null) {
+            throw new RuntimeException("로그인이 필요한 서비스 입니다.");
+        }
+        return sessionUser;
+    }
+}
