364bcba055ef33371bdb8ecbbe461bb81222e9df
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-11-14 15:47:01 +0900

    Add: 로그인 기능 추가 [#3]

--- src/main/java/gg/loto/user/service/UserService.java
+++ src/main/java/gg/loto/user/service/UserService.java
@@ -1,7 +1,7 @@
 package gg.loto.user.service;
 
 import gg.loto.user.web.dto.UserUpdateRequest;
-import jakarta.transaction.Transactional;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -10,16 +10,17 @@ import gg.loto.user.web.dto.UserResponse;
 import gg.loto.user.web.dto.UserSaveRequest;
 import gg.loto.user.domain.User;
 import gg.loto.user.repository.UserRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
-
+    
+    @Transactional
     public UserResponse signUp(UserSaveRequest userSaveRequest) {
         
         validateDuplicateEmail(userSaveRequest.getEmail());
@@ -29,11 +30,13 @@ public class UserService {
         return UserResponse.of(savedUser);
     }
 
+    @Transactional(readOnly = true)
     public void validateDuplicateEmail(String email) {
         if (userRepository.findByEmail(email).isPresent()) 
             throw new RuntimeException("이미 존재하는 이메일입니다.");
     }
 
+    @Transactional
     public UserResponse updateProfile(Long id, UserUpdateRequest userUpdateRequest) {
         if ( userUpdateRequest.hasNoChanges() )
             throw new RuntimeException("잘못된 수정 요청입니다.");
@@ -50,5 +53,11 @@ public class UserService {
         userRepository.save(user);
 
         return UserResponse.of(user);
+    }
+    
+    @Transactional(readOnly = true)
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("회원정보를 찾을 수 없습니다."));
     }
 }
