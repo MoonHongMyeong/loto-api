0055a1c7feb3647a10b8be0593175d0260a38495
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-11-15 12:49:04 +0900

    Modify: openApi response에 맞춰서 엔티티 수정 [#4]

--- src/main/java/gg/loto/character/domain/Characters.java
+++ src/main/java/gg/loto/character/domain/Characters.java
@@ -3,11 +3,14 @@ package gg.loto.character.domain;
 import gg.loto.global.entity.BaseEntity;
 import gg.loto.user.domain.User;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @Table(name = "characters")
+@NoArgsConstructor
 public class Characters extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,15 +21,36 @@ public class Characters extends BaseEntity {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @Column(name = "name", nullable = false, unique = true)
-    private String name;
+    @Column(name = "server_name", nullable = false)
+    private String serverName;
 
-    @Column(name = "class_name", nullable = false)
-    private String className;
+    @Column(name = "character_name", nullable = false, unique = true)
+    private String characterName;
 
-    @Column(name = "item_level", nullable = false)
-    private float itemLevel;
+    @Column(name = "character_class_name", nullable = false)
+    private String characterClassName;
+
+    @Column(name = "item_max_level", nullable = false)
+    private String itemMaxLevel;
+
+    @Column(name = "item_avg_level", nullable = false)
+    private String itemAvgLevel;
+
+    @Column(name = "character_level", nullable = false)
+    private int characterLevel;
     
     @Column(name = "character_image")
     private String characterImage;
+
+    @Builder
+    public Characters(User user, String characterName, String serverName, String characterClassName, String itemMaxLevel, String itemAvgLevel, int characterLevel, String characterImage) {
+        this.user = user;
+        this.characterName = characterName;
+        this.serverName = serverName;
+        this.characterClassName = characterClassName;
+        this.itemMaxLevel = itemMaxLevel;
+        this.itemAvgLevel = itemAvgLevel;
+        this.characterLevel = characterLevel;
+        this.characterImage = characterImage;
+    }
 }
