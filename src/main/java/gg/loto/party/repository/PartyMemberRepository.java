17d9d2aec77b10f727c13722a2f04e1280268670
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-11-23 02:16:36 +0900

    Add: PartyMember 객체는 Users.id를 가지고 있지 않기 때문에 JPQL 쿼리 메서드 작성 [#5]

--- src/main/java/gg/loto/party/repository/PartyMemberRepository.java
+++ src/main/java/gg/loto/party/repository/PartyMemberRepository.java
@@ -3,11 +3,15 @@ package gg.loto.party.repository;
 import gg.loto.party.domain.PartyMember;
 import gg.loto.party.domain.PartyMemberId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Set;
 
 public interface PartyMemberRepository extends JpaRepository<PartyMember, PartyMemberId> {
     void deleteByPartyIdAndCharacterIdIn(Long partyId, Set<Long> characters);
 
-    void deleteByPartyIdAndUserId(Long id, Long userId);
+    @Query("DELETE FROM PartyMember pm WHERE pm.party.id = :partyId AND pm.character.user.id = :userId")
+    @Modifying
+    void deleteByPartyIdAndUserId(Long partyId, Long userId);
 }
