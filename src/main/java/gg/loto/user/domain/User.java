24ee71bb9a95440331460d74b5c5d54eb1790e3c
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-11-28 14:07:01 +0900

    Add: 로그인 방식을 OAuth2 변환하면서 필요없어진 컬럼 삭제 [#6]

--- src/main/java/gg/loto/user/domain/User.java
+++ src/main/java/gg/loto/user/domain/User.java
@@ -16,40 +16,28 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "email", unique = true, nullable = false)
-    private String email;
-
-    @Column(name = "password", nullable = false)
-    private String password;
-
     @Column(name = "nickname", nullable = false)
     private String nickname;
 
     @Column(name = "api_key", length = 500)
     private String apiKey;
 
-    @Column(name = "discord_id")
+    @Column(name = "discord_id", nullable = false)
     private String discordId;
 
-    @Column(name = "discord_username")
+    @Column(name = "discord_username", nullable = false)
     private String discordUsername;
 
     @Column(name = "discord_avatar")
     private String discordAvatar;
 
     @Builder
-    public User(String email, String password, String nickname, String apiKey, String discordId, String discordUsername, String discordAvatar) {
-        this.email = email;
-        this.password = password;
+    public User(String nickname, String apiKey, String discordId, String discordUsername, String discordAvatar) {
         this.nickname = nickname;
         this.apiKey = apiKey;
         this.discordId = discordId;
         this.discordUsername = discordUsername;
         this.discordAvatar = discordAvatar;
-    }
-
-    public void changePassword(String newPassword) {
-        this.password = newPassword;
     }
 
     public void changeNickname(String newNickname) {
