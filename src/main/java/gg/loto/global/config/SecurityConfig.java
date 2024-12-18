4d4d8dd13ae8dc267503a13cf63962aece3c5c2b
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-12-18 12:13:05 +0900

    Add: 디스코드 로그인, 토큰 refresh 요청을 제외한 다른 요청에 모두 토큰을 포함하도록 spring security 설정 [#6]

--- src/main/java/gg/loto/global/config/SecurityConfig.java
+++ src/main/java/gg/loto/global/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -24,7 +23,7 @@ public class SecurityConfig {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**", "/api/v1/invite-code/**")
+                        .requestMatchers("/api/v1/auth/discord/login","/api/v1/auth/token/refresh")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
