8230911185c7f512d77fa22ea8e43d3a8963a977
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-12-18 13:52:34 +0900

    Modify: 사용하지 않는 email 인자 제거 [#3]

--- src/main/java/gg/loto/user/repository/UserRepository.java
+++ src/main/java/gg/loto/user/repository/UserRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import gg.loto.user.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-
     Optional<User> findByDiscordId(String discordId);
 }
